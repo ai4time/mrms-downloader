9059438d7455381228aa2d49cb100e523da6ccfe
yipengh <huang.ypeng@gmail.com>
2023-05-07 21:15:41 +0800

    Retry when 404 & dynamic waiting time

--- mrms/downloader.py
+++ mrms/downloader.py
@@ -4,20 +4,25 @@ from datetime import datetime, timedelta, timezone
 from pathlib import Path
 
 import requests
+from requests.exceptions import HTTPError
 
 from mrms.logger import logger
 
 
 class MRMSDownloader:
+
+    def __init__(self, base_dir: os.PathLike="data"):
+        self.base_dir = Path(base_dir)
+
     def run(self):
         while True:
-            self._poll()
-            time.sleep(600)
+            _wait = self._poll()
+            time.sleep(_wait)
 
     def _poll(self):
         ### Note: all times should be in UTC
         ### Note: there is a 2-minute delay for a file to be online
-        now = datetime.utcnow() - timedelta(minutes=2)
+        now = datetime.now(timezone.utc) - timedelta(minutes=2)
 
         # Round down to nearest 10 minutes
         last = datetime(
@@ -35,12 +40,27 @@ class MRMSDownloader:
         filename = f"MRMS_PrecipRate_00.00_{dt_str}.grib2.gz"
         url = f"https://mrms.ncep.noaa.gov/data/2D/PrecipRate/{filename}"
 
-        base_dir = Path("data")
-        save_dir = base_dir / str(last.year) / f"{last.month:02d}{last.day:02d}"
-        save_dir.mkdir(parents=True, exist_ok=True)
-
+        save_dir = self._ensure_save_dir(dt=last)
         save_path = save_dir / filename
-        self._download(url, save_path)
+
+        try:
+            self._download(url, save_path)
+            return self._wait_time(now=now, last=last)
+        except HTTPError as e:
+            _status = e.response.status_code
+            logger.error(
+                f"Failed to download {url}: "
+                f"[{_status}] {e.response.reason}"
+            )
+            if _status == 404:
+                # Possibly a time nuance that the file is not online yet
+                # Just wait for a short time and retry
+                return 60
+
+    def _ensure_save_dir(self, dt: datetime) -> os.PathLike:
+        save_dir = self.base_dir / str(dt.year) / f"{dt.month:02d}{dt.day:02d}"
+        save_dir.mkdir(parents=True, exist_ok=True)
+        return save_dir
 
     def _download(self, url: str, save_path: os.PathLike) -> None:
         logger.info(f"Downloading {url}...")
@@ -49,3 +69,9 @@ class MRMSDownloader:
         with open(save_path, 'wb') as f:
             f.write(res.content)
         logger.info(f"Saved to {save_path}")
+
+    def _wait_time(self, now: datetime, last: datetime) -> int:
+        # Wait until the next 10-minute mark
+        next = last + timedelta(minutes=10)
+        wait_time = (next - now).total_seconds()
+        return int(wait_time)
